f68c7e4433c54111cad4b5da3788d5b28710480f
RamandeepNahar <ramandeep.s.nahar@gmail.com>
2024-09-15 12:35:19 -0400

    Added Button, when pressed message changes to "Clicked!"

--- app/src/main/java/edu/temple/baseassignment/MainActivity.kt
+++ app/src/main/java/edu/temple/baseassignment/MainActivity.kt
@@ -2,11 +2,17 @@ package edu.temple.baseassignment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textview)
+        findViewById<View>(R.id.button).setOnClickListener{
+            textView.text = "Clicked!"
+        }
     }
 }
